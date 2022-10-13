459d882d71372696f6377ceb0aa0a328f73c9a70
PremKumbhar <kumbharprem7738@gmail.com>
2022-10-13 18:30:31 +0530

    [Prem]:Added basic information in contact class

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,151 @@
+package com.bridgelabz;
+
+public class Contact {
+	private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNo;
+    private String email;
+
+    public Contact(){};
+
+    public Contact(String firstName, String lastName, String street, String city, String state, int zip, long phoneNo, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public int getZip() {
+		return zip;
+	}
+
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	/**
+	 * @return the phoneNo
+	 */
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	/**
+	 * @param phoneNo the phoneNo to set
+	 */
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ","
+						+ " lastName=" + lastName + ", "
+						+ "street=" + street + ","
+						+ " city=" + city
+						+ ", state=" + state + ", "
+						+ "zip=" + zip + ", "
+						+ "phoneNo=" + phoneNo + ","
+						+ " email=" + email + "]";
+	}
+    
+    
+}
