714bedb3a77b91dbbc61b13702c9bb0102aeee75
PremKumbhar <kumbharprem7738@gmail.com>
2022-10-13 19:26:00 +0530

    [Prem]:Added delete contact file

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -10,7 +10,7 @@ public class AddressBookMain {
 		
 		do {
 			System.out.println("***Welcome to Address Book Program***");
-			System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EDIT CONTACT \n4. EXIT ");
+			System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EDIT CONTACT \n4. DELETE CONTACT \n5. EXIT ");
 			System.out.println("Enter the Operation Number");
 			operationNo = sc.nextInt();
 			
@@ -24,10 +24,13 @@ public class AddressBookMain {
 			case 3:
 				addressBook.editContact();
 				break;
+			case 4:
+				addressBook.deleteContact();
+				break;
 			default:
 				System.out.println("Wrong Operation Number");	
 				break;
 			}
-		} while (operationNo != 4);
+		} while (operationNo != 5);
 	}
 }
